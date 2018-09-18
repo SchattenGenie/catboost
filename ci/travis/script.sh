e2ce32c8e035295ab97778e5ca3a59646c20aa34
Belavin Vladislav <belavin@phystech.edu>
2018-09-18 21:50:46 +0300

    meet deps

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -18,7 +18,7 @@ function install_cuda_linux()
     sudo dpkg -i ${CUDA_REPO_PKG}
     sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
     sudo apt-get update
-    sudo apt-get -y install cuda-9-0
+    sudo apt-get -y install cuda-runtime-9-0 cuda-demo-suite-9-0 cuda-9-0
     export PATH=${PATH}:/usr/local/cuda-9.0/bin
     export CUDA_HOME=${CUDA_HOME}:/usr/local/cuda:/usr/local/cuda-9.0
     export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda-9.0/lib64
