4ea5968287ba8d233fe7fca80ae204533842241e
Belavin Vladislav <belavin@phystech.edu>
2018-09-19 20:28:01 +0300

    e

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -11,26 +11,15 @@ function install_cuda_linux()
     sudo dpkg -i ${CUDA_REPO_PKG}
     sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
     sudo apt-get update
-    sudo apt-get -y install nvidia-settings cuda-drivers cuda-runtime-9-0 cuda-demo-suite-9-0 cuda-9-0
-    export PATH=${PATH}:/usr/local/cuda-9.0/bin
-    export CUDA_HOME=${CUDA_HOME}:/usr/local/cuda:/usr/local/cuda-9.0
-    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda-9.0/lib64
+    sudo apt-get install cuda-9-0 # nvidia-settings cuda-drivers cuda-runtime-9-0 cuda-demo-suite-9-0 
+    # export PATH=${PATH}:/usr/local/cuda-9.0/bin
+    # export CUDA_HOME=${CUDA_HOME}:/usr/local/cuda:/usr/local/cuda-9.0
+    # export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda-9.0/lib64
 }
 
 
 if [ "${CB_BUILD_AGENT}" == 'clang-linux-x86_64-release-cuda' ]; then
      install_cuda_linux;
-     echo $CXX
-     echo $CC
-     chmod +x /home/travis/build/SchattenGenie/catboost/catboost/python-package/mk_wheel.py
-     chmod +x /home/travis/virtualenv/python2.7.14/bin/python2
-     chmod +x /home/travis/build/SchattenGenie/catboost/ya
-     ls -lta
-     ls -lta /home/travis/virtualenv/python2.7.14/bin/
-     ls -ltaR /home/travis/.ya/tools/v3/
-     whoami
-     #sudo chown -R frappe:frappe *
-     #chmod +x /home/travis/.ya/tools/v3/
      ./ya make --no-emit-status --stat -T -r -j 1 catboost/app -DCUDA_ROOT=/usr/local/cuda-9.0 -DNO_DEBUGINFO;
      cp $(readlink -f catboost/app/catboost) catboost-cuda-linux;
      python ci/webdav_upload.py catboost-cuda-linux
@@ -39,14 +28,6 @@ fi
 if [ "${CB_BUILD_AGENT}" == 'python2-linux-x86_64-release' ]; then
      # install_cuda_linux;
      cd catboost/python-package;
-     echo $CXX
-     echo $CC
-     ls -lta
-     ls -lta /home/travis/virtualenv/python2.7.14/bin/
-     #ls -lta /usr/local/cuda-9.0
-     ls -lta /home/travis/build/SchattenGenie/catboost/
-     ls -lta /home/travis/build/SchattenGenie/catboost/catboost/python-package/
-     whoami
      python2 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-9.0 ;
      python ../../ci/webdav_upload.py *.whl
 fi
