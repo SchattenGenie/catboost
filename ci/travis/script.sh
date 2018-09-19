d60161c61ef6bcc9cec5cd545ba8391acce73b5a
vbelavin <belavin@phystech.edu>
2018-09-19 09:27:37 +0300

    py3.4 xenial -> trusty, cuda off

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -42,9 +42,9 @@ fi
 if [ "${CB_BUILD_AGENT}" == 'python34-linux-x86_64-release' ]; then
      ls /home/travis/virtualenv
      ln -s /home/travis/virtualenv/python3.4.6/bin/python-config /home/travis/virtualenv/python3.4.6/bin/python3-config;
-     install_cuda_linux;
+     # install_cuda_linux;
      cd catboost/python-package;
-     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-9.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.4.6/bin/python3-config;
+     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.4.6/bin/python3-config;
      python ../../ci/webdav_upload.py *.whl
 fi
 
